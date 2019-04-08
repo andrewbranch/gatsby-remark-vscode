604a63c9710f2336bb34007c7cbe655ea392f36f
Andrew Branch <andrew@wheream.io>
2019-04-07 20:30:42 -0700

    Add code block options parser

---
+++ src/parseCodeFenceHeader.js
@@ -0,0 +1,157 @@
+// @ts-check
+const identifierPattern = /[a-z0-9-–—_]/i;
+const triviaPattern = /\s/;
+const startOfNumberPattern = /[0-9-.]/;
+const numberPattern = /[0-9-.e]/;
+
+/**
+ * @param {string} input
+ * @param {RegExp} pattern
+ */
+function test(input, pattern) {
+  if (input === undefined) return false;
+  return pattern.test(input);
+}
+
+/**
+ * @param {string} input
+ */
+function parseCodeFenceHeader(input) {
+  let pos = 0;
+  let options = {};
+  const languageName = parseIdentifier();
+  skipTrivia();
+  if (!isEnd() && current() === '{') {
+    options = parseObject();
+  }
+
+  return { languageName, options };
+
+
+  
+  function current() {
+    if (isEnd()) {
+      return fail('Unexpected end of input');
+    }
+    return input[pos];
+  }
+
+  function isEnd() {
+    return pos >= input.length;
+  }
+
+  /**
+   * @param {string} message
+   * @returns {never}
+   */
+  function fail(message) {
+    throw new Error(`Failed parsing code fence header '${input}' at position ${pos}: ${message}`);
+  }
+
+  /**
+   * @param {string} expected 
+   */
+  function scanExpected(expected) {
+    if (isEnd() || current() !== expected) {
+      return fail(`Expected '${expected}'`);
+    }
+    pos++;
+  }
+
+  function parseIdentifier(errorMessage = 'Expected identifier, but got nothing') {
+    let identifier = '';
+    while (!isEnd() && test(current(), identifierPattern)) {
+      identifier += current();
+      pos++;
+    }
+    if (!identifier) {
+      return fail(errorMessage);
+    }
+    return identifier;
+  }
+
+  function skipTrivia() {
+    while (!isEnd() && test(current(), triviaPattern)) pos++;
+  }
+
+  function parseChar() {
+    let char = current();
+    if (char === '\\') {
+      pos++;
+      char += current();
+    }
+    pos++;
+    return char;
+  }
+
+  function parseString() {
+    let str = '';
+    const quote = current();
+    pos++;
+    while (true) {
+      const char = parseChar();
+      if (char === quote) break;
+      str += char.replace(/\\/, '');
+    }
+    return str;
+  }
+
+  function parseNumber() {
+    let numStr = current();
+    pos++;
+    while (!isEnd() && test(current(), numberPattern)) {
+      numStr += current();
+      pos++;
+    }
+    return parseFloat(numStr);
+  }
+
+  function parseBoolean() {
+    const identifier = parseIdentifier('Expected expression, but got nothing');
+    switch (identifier) {
+      case 'true': return true;
+      case 'false': return false;
+      case '': return fail('Expected expression, but got nothing');
+      default: return fail(`Unrecognized input '${identifier}'`);
+    }
+  }
+
+  function parseExpression() {
+    switch (current()) {
+      case '{':
+        return parseObject();
+      case `'`:
+      case '"':
+        return parseString();
+    }
+    if (test(current(), startOfNumberPattern)) {
+      return parseNumber();
+    }
+    return parseBoolean();
+  }
+
+  function parseObject() {
+    let obj = {};
+    scanExpected('{');
+    skipTrivia();
+    while (!isEnd() && current() !== '}') {
+      const key = parseIdentifier();
+      /** @type {*} */
+      let value = true;
+      skipTrivia();
+      if (current() === ':') {
+        pos++;
+        skipTrivia();
+        value = parseExpression();
+        skipTrivia();
+      }
+      obj[key] = value;
+      if (current() === ',') pos++;
+      skipTrivia();
+    }
+    scanExpected('}');
+    return obj;
+  }
+}
+
+module.exports = parseCodeFenceHeader;
---
+++ test/parseCodeFenceHeader.test.js
@@ -0,0 +1,56 @@
+// @ts-check
+const parse = require('../src/parseCodeFenceHeader');
+
+describe('parseCodeFenceHeader', () => {
+  it('parses language name without options', () => {
+    expect(parse('jsx')).toEqual({ languageName: 'jsx', options: {} });
+  });
+
+  it('parses empty options', () => {
+    expect(parse('jsx{}')).toEqual({ languageName: 'jsx', options: {} });
+  });
+
+  it('ignores space between language name and options', () => {
+    expect(parse('jsx     {}')).toEqual({ languageName: 'jsx', options: {} });
+  });
+
+  it('ignores space within options object', () => {
+    expect(parse('jsx{    }')).toEqual({ languageName: 'jsx', options: {} });
+  });
+
+  it('parses gatsby-remark-prismjs line highlighting syntax', () => {
+    expect(parse('jsx{1,4-6}')).toEqual({ languageName: 'jsx', options: { 1: true, '4-6': true } });
+  });
+
+  it('ignores space between object keys', () => {
+    expect(parse('jsx{1,    4-6}')).toEqual({ languageName: 'jsx', options: { 1: true, '4-6': true } });
+  });
+
+  it('parses objects with number values', () => {
+    expect(parse('jsx{a: 24e3, b: .9, c: -4.2}'))
+      .toEqual({ languageName: 'jsx', options: { a: 24e3, b: 0.9, c: -4.2 } });
+  });
+
+  it('parses objects with string values', () => {
+    expect(parse(`jsx{a: "b", c: 'd'}`)).toEqual({ languageName: 'jsx', options: { a: 'b', c: 'd' } });
+  });
+
+  it('parses objects with boolean values', () => {
+    expect(parse('jsx{a: true, b: false}')).toEqual({ languageName: 'jsx', options: { a: true, b: false } });
+  });
+
+  it('parses nested objects', () => {
+    expect(parse('jsx{ a: { 1 } }')).toEqual({ languageName: 'jsx', options: { a: { 1: true } } });
+  });
+
+  it('parses strings with escape characters', () => {
+    expect(parse(`jsx{a: '\\''}`)).toEqual({ languageName: 'jsx', options: { a: `'` } });
+  });
+
+  it('gives good error messages', () => {
+    expect(() => parse('jsx{ a: }')).toThrowError(/expected expression/i);
+    expect(() => parse('jsx{ a: boo }')).toThrowError(/unrecognized input 'boo'/i);
+    expect(() => parse('jsx{ : }')).toThrowError(/expected identifier/i);
+    expect(() => parse('jsx{ a: "')).toThrowError(/unexpected end of input/i);
+  });
+});
