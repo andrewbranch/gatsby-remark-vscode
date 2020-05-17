05fe6ff942952ce47f80905360c0163b28556d2b
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-05-17 07:45:26 +0200

    diffLineTransformer return new v3 gutterCells prop

--- src/transformers/diffLineTransformer.js
+++ src/transformers/diffLineTransformer.js
@@ -4,30 +4,44 @@ const { highlightDiffLine } = require('./transformerUtils');
 function createDiffLineTransformer() {
   // How do I get the parsed code fence options here?
   /** @type {LineTransformer<any>} */
-  const transformer = ({ line, parsedOptions: { diff } }) => {
+  const diffLineTransformer = ({ line, meta: { diff } }) => {
     if (diff && line.text.startsWith('+ ')) {
+      line.text = line.text.replace(/^\+ /, '');
       return {
         line: highlightDiffLine(line, 'add'),
-        data: { isDiffAdd: true }
+        data: { isDiffAdd: true },
+        gutterCells: [
+          {
+            className: 'grvsc-diff-add',
+            text: `+`
+          }
+        ]
       };
     }
     if (diff && line.text.startsWith('- ')) {
+      line.text = line.text.replace(/^\- /, '');
       return {
         line: highlightDiffLine(line, 'del'),
-        data: { isDiffDel: true }
+        data: { isDiffDel: true },
+        gutterCells: [
+          {
+            className: 'grvsc-diff-del',
+            text: `-`
+          }
+        ]
       };
     }
     return { line };
   };
 
-  transformer.displayName = 'diff';
-  transformer.schemaExtension = `
+  diffLineTransformer.displayName = 'diff';
+  diffLineTransformer.schemaExtension = `
     type GRVSCTokenizedLine {
       isDiff: Boolean
     }
   `;
 
-  return transformer;
+  return diffLineTransformer;
 }
 
 module.exports = { createDiffLineTransformer };
